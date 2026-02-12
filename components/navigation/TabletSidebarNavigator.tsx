b9320d41c09077941ac2dedba6474af1bf67849c
无名通讯科技 <34734036+203535211@users.noreply.github.com>
2026-02-12 10:18:32 +0800

    Change app title from 'OrionTV' to '云朵TV'

--- components/navigation/TabletSidebarNavigator.tsx
+++ components/navigation/TabletSidebarNavigator.tsx
@@ -136,7 +136,7 @@ const TabletSidebarNavigator: React.FC<TabletSidebarNavigatorProps> = ({
             )}
           </TouchableOpacity>
           {!collapsed && (
-            <ThemedText style={dynamicStyles.appTitle}>OrionTV</ThemedText>
+            <ThemedText style={dynamicStyles.appTitle}>云朵TV</ThemedText>
           )}
         </View>
 
